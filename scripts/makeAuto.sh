9fb61995d9533430d0d8248c22525e8a250bc9f7
newtykins <newtykins@outlook.com>
2023-03-05 00:25:38 +0000

    chore: remove old scripts

--- scripts/makeAuto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-out=$(pwd)/out
-mkdir $out/auto
-cp $(pwd)/../../prism/{.,}* $out/auto 2> /dev/null
-cp $(pwd)/../../prism/$1/{.,}* $out/auto 2> /dev/null
-zip $out/auto.zip $out/auto/*
-rm -rf $out/auto
--- scripts/outExists.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-out=$(pwd)/out
-
-if [ -d $out ]; then
-	rm -rf $out
-fi
-
-mkdir $out
